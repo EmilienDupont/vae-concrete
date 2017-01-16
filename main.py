4b45c9cab75c1186a2c792c4e5902909eb510bfc
EmilienDupont <emilien.dupont@ymail.com>
2017-01-15 18:40:14 -0800

    :camel: cleaner plotting

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from vae_concrete import VAE
 (x_train, y_train), (x_test, y_test) = get_processed_mnist()
 model = VAE(latent_disc_dim=10)
 model.fit(x_train, num_epochs=1)
-model.plot()
+model.plot(std_dev=1.)
